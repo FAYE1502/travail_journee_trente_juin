fec1aa6422749dacd7e8c82e7f61996707a7440e
FAYE1502 <fayemodou1502@gmail.com>
2026-06-30 23:09:31 +0000

    tableau de categories chaque categorie est caracterisé par son code, son nom et ses liste de produits.chaque produit est caracterisé par son nom, sa reference, son prix et sa quantité

---
+++ imperatifs.php
@@ -0,0 +1,34 @@
+<?php
+// 1.
+$categories = [
+
+ 0=> [
+    'code' => 'AZE0',
+    'nom' => 'categ0',
+    'listeDesProduits' =>[  
+    0=>[
+    'nom' => 'prod0',
+    'ref' => 'ref0',
+    'prix' => '2500',
+    'quantite' => '9'
+    ],
+
+    1 =>[
+    'nom' => 'prod1',
+    'ref' => 'ref1',
+    'prix' => '8800',
+    'quantite' => '6'
+    ]]
+
+],
+
+  1=> [
+    'code' => 'AZE1',
+    'nom' => 'categ1',
+    'listeDesProduits' => []
+    
+
+],
+
+
+];
